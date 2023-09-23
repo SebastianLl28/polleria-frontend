adda97762118169c27831653d69033739df58f70
SebastianLl28 <llamucasebas@gmail.com>
2023-09-23 07:13:23 -0500

    feat: add router home and hero to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { Button } from "@mui/material";
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/index";
+import { Box } from "@mui/material";
 
 const App = () => {
-  return <main></main>;
+  return (
+    <Box
+      sx={{ backgroundColor: "#f3f3f3", minHeight: "100vh" }}
+      component="main"
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </Box>
+  );
 };
 
 export default App;
