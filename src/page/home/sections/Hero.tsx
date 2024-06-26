89e4c5637164e9d59b65271a51b3092855045304
Brand0n19 <bfarfanllaulle@gmail.com>
2024-06-26 09:44:54 -0500

    ommit

--- src/page/home/sections/Hero.tsx
+++ src/page/home/sections/Hero.tsx
@@ -16,21 +16,21 @@ const Hero = () => {
         <CarouselContent>
           <CarouselItem>
             <img
-              className='w-full'
+              className='w-full min-h-64'
               src='https://s3-rokys-pro.s3.amazonaws.com/media/wysiwyg/1710792531_banner_1200px_x_375px.jpg'
               alt='pollito'
             />
           </CarouselItem>
           <CarouselItem>
             <img
-              className='w-full'
+              className='w-full min-h-64'
               src='https://s3-rokys-pro.s3.amazonaws.com/media/wysiwyg/1693495675_banner-desktop-24-horas-nuevo.jpg'
               alt='pollito'
             />
           </CarouselItem>
           <CarouselItem>
             <img
-              className='w-full'
+              className='w-full min-h-64'
               src='https://s3-rokys-pro.s3.amazonaws.com/media/wysiwyg/1699897692_ROKY_BURGER_HOT_CRUNCH_cabecera_web_-_desktop.jpg'
               alt='pollitos'
             />
